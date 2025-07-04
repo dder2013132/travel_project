6366b2e6b607fe5a79ec6c20e6af68441579c6bf
dder2013132 <dder2013@gmail.com>
2025-07-04 17:41:40 +0900

    new_commit_for_test

--- travel-project/src/app/test/page.tsx
+++ travel-project/src/app/test/page.tsx
@@ -84,8 +84,7 @@ export default function Home() {
         </a>
         <a
           className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
+          href="http://localhost:3000"
           rel="noopener noreferrer"
         >
           <Image
@@ -95,22 +94,7 @@ export default function Home() {
             width={16}
             height={16}
           />
-          Go to nextjs.org →
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="http://localhost:3000/test"
-          // target="_blank" -> 새 창으로 열기
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          test
+          Go home
         </a>
       </footer>
     </div>
